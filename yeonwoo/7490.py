0adde6278f3c8cf7255d50b6dbb44cbb10b2ac6d
박연우 <parkyeonwoo@bag-yeon-uui-MacBookAir.local>
2024-07-15 20:13:59 +0900

    누락된 것들 그냥 추가...

--- yeonwoo/7490.py
+++ yeonwoo/7490.py
@@ -5,6 +5,29 @@ from itertools import product
 
 M = int(input()) # 테스트 케이스의 숫자
 
+def calculate(numbers, operation): # list로 나타내져 있는 수식을 계산해주는 함수
+
+    sum = 0
+    num = 0
+    ope = 1
+    for i in range(N-1):
+        num += int(numbers[i])
+        
+        if operation[i] == '+':
+            sum += num * ope
+            ope = 1
+            num = 0
+        elif operation[i] == '-':
+            sum += num * ope
+            ope = -1
+            num = 0
+        elif operation[i] == ' ':
+            num *= 10
+    num += int(numbers[N-1])
+    sum += num * ope
+
+    return sum
+
 for _ in range(M): # 각 테스트 케이스마다 실행
     N = int(input())
     outputs = [] # 수식의 결과가 0이 되는 수식
